c65c2eec9c87a3bbfe871d75e1c639913879fd07
Finley Edwards <EDW17006550@btc.ac.uk>
2022-05-17 12:15:20 +0100

    Added tenant class

---
+++ SCDT41 CW2/Tenant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCDT41_CW2
+{
+    internal class Tenant
+    {
+        public Guid Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+
+        public Tenant(Guid id, string fname, string lname)
+        {
+            Id = id;
+            Fname = fname;
+            Lname = lname;
+        }
+    }
+}
